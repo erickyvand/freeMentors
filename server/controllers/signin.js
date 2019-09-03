7c04f5e7cd3309c763552fac837516ea3a59bf0c
Ericky Vand <erickyvand@gmail.com>
2019-09-03 20:04:39 +0200

    chore(remove firstName and lastName on response): update signin file [finishes #168272294]

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -31,8 +31,6 @@ router.post('/signin', (req, res) => {
             message: `User ${user.first_name} ${user.last_name} is successfully logged in`,
             data: {
               token: token,
-              first_name: user.first_name,
-              last_name: user.last_name,
             },
           });
         });
