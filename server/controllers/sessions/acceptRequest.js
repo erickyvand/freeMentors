f7cf86f54eeb4464bcc12004ddb752e4ede81436
Ericky Vand <erickyvand@gmail.com>
2019-09-10 16:26:28 +0200

    feature(accept mentorship session): add accept request file [finishes #168363818]

--- server/controllers/sessions/acceptRequest.js
+++ server/controllers/sessions/acceptRequest.js
@@ -4,63 +4,66 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import verifyToken from '../../middleware/verifyToken';
-import sessions from '../../models/session';
+import client from '../../config/config';
 
 const router = express.Router();
 
 router.patch('/sessions/:sessionId/accept', verifyToken, (req, res) => {
   jwt.verify(req.token, process.env.AUTH_KEY, (err, loggedUser) => {
-    const request = sessions.find((c) => c.sessionId === parseInt(req.params.sessionId));
-
     if (err) {
       res.status(403).json({
         status: 403,
         error: 'Forbidden',
-      });
-    // check if logged user is a mentor  
-    } else if (loggedUser.user.user_type !== 'mentor') {
-      res.status(403).json({
-        status: 403,
-        error: 'You are not allowed to access this route',
-      });
-    // check if the session id exists  
-    } else if (!request) {
-      res.status(404).json({
-        status: 404,
-        error: 'The given session Id does not exists',
-      });
-    // make sure logged user must update his request  
-    } else if (loggedUser.user.id !== request.mentorId) {
-      res.status(403).json({
-        status: 403,
-        error: 'The given session ID is not your request, The logged Mentor Id and requested Mentor Id does not match',
-      });
-    // check if the request was not rejected before  
-    } else if (request.status === 'rejected') {
-      res.status(401).json({
-        status: 401,
-        error: 'This request can not be accepted because was rejected before',
-      });
-    // check if the request was not accepted before  
-    } else if (request.status === 'accepted') {
-      res.status(409).json({
-        status: 409,
-        error: 'This request was made before',
-      });
+      });  
     } else {
-      // update status to accepted
-      // eslint-disable-next-line no-unused-expressions
-      request.status = 'accepted',
-
-      res.status(200).json({
-        message: 'The request was successfully accepted',
-        status: 200,
-        data: {
-          sessionId: request.sessionId,
-          menteeEmail: loggedUser.user.email,
-          questions: request.questions,
-          status: request.status,
-        },
+      client.query('SELECT * FROM request_session WHERE id = $1', [req.params.sessionId]).then((results) => {
+        // check if logged user is a mentor
+        if (loggedUser.userIn.user_type !== '2') {
+          res.status(403).json({
+            status: 403,
+            error: 'You are not allowed to access this route',
+          });
+        // verify if ID exists   
+        } else if (results.rows === 'undefined' || results.rows.length === 0) {
+          res.status(404).json({
+            status: 404,
+            error: 'The given request session Id does not exists',
+          });
+        // logged mentor Id should match request mentor Id  
+        } else if (loggedUser.userIn.id !== results.rows[0].mentorid) {
+          res.status(403).json({
+            status: 403,
+            error: 'The given session ID is not your request, The logged Mentor Id and requested Mentor Id does not match',
+          });
+        // check if they were no reject before  
+        } else if (results.rows[0].status === 'rejected') {
+          res.status(401).json({
+            status: 401,
+            error: 'This request can not be accepted because was rejected before',
+          });
+        // check if the accepted wasn't made before  
+        } else if (results.rows[0].status === 'accepted') {
+          res.status(409).json({
+            status: 409,
+            error: 'This request was made before',
+          });
+        // update if everything goes right  
+        } else {
+          client.query('UPDATE request_session SET status = \'accepted\' WHERE id = $1', [req.params.sessionId]);
+          client.query('SELECT r.id as sessionid, r.mentorid as mentorId, u.id as menteeId, r.questions as questions, u.email as email, r.status as status FROM users u JOIN request_session r ON u.id = r.menteeId WHERE r.id = $1 ORDER BY r.id DESC LIMIT 1', [req.params.sessionId]).then((accept) => {
+            res.status(200).json({
+              status: 200,
+              data: {
+                sessionId: accept.rows[0].sessionid,
+                mentorId: accept.rows[0].mentorid,
+                menteeId: accept.rows[0].menteeid,
+                questions: accept.rows[0].questions,
+                email: accept.rows[0].email,
+                status: accept.rows[0].status, 
+              },
+            });
+          });
+        } 
       });
     }
   });
