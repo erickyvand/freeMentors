eaeaa9f90bab29312a09793958241d892097034a
Ericky Vand <erickyvand@gmail.com>
2019-09-11 00:24:01 +0200

    chore(test for all users): add all users test file [finishes #168417220]

--- server/tests/usersTests/allUsersTest.js
+++ server/tests/usersTests/allUsersTest.js
@@ -5,15 +5,14 @@ import chaihttp from 'chai-http';
 import app from '../../../app';
 import token from '../helpers/dummyToken';
 
-// eslint-disable-next-line no-unused-vars
-const should = chai.should();
+chai.should();
 
 chai.use(chaihttp);
 
 describe('/GET All Users and all Mentors separately', () => {
   it('App should check for set headers token', (done) => {
     chai.request(app)
-      .get('/api/v1/users')
+      .get('/api/v2/users')
       .then((res) => {
         res.body.status.should.be.equal(403);
         done();
@@ -22,7 +21,7 @@ describe('/GET All Users and all Mentors separately', () => {
 
   it('App should check token is valid', (done) => {
     chai.request(app)
-      .get('/api/v1/users')
+      .get('/api/v2/users')
       .set('Authorization', token.fakeAdminToken)
       .then((res) => {
         res.body.status.should.be.equal(403);
@@ -33,7 +32,7 @@ describe('/GET All Users and all Mentors separately', () => {
 
   it('App should check route access', (done) => {
     chai.request(app)
-      .get('/api/v1/users')
+      .get('/api/v2/users')
       .set('Authorization', token.userToken)
       .then((res) => {
         res.body.status.should.be.equal(403);
@@ -44,7 +43,7 @@ describe('/GET All Users and all Mentors separately', () => {
 
   it('App should get users and mentors', (done) => {
     chai.request(app)
-      .get('/api/v1/users')
+      .get('/api/v2/users')
       .set('Authorization', token.adminToken)
       .then((res) => {
         res.body.status.should.be.equal(200);
