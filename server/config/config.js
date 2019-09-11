0881ba5a108ff08e50d2c0cd23d1b822f945076a
Ericky Vand <erickyvand@gmail.com>
2019-09-11 11:48:11 +0200

    update file

--- server/config/config.js
+++ server/config/config.js
@@ -3,14 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+const connectionString = process.env.DATABASE_URL;
 
-client.connect();
+const client = new Client({
+  connectionString,
+});
 
 module.exports = client;
