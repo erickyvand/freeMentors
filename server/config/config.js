c501844fc8c3041cf72bd00aaac43cbc7be4ead6
Ericky Vand <erickyvand@gmail.com>
2019-09-11 13:11:08 +0200

    update file

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
+
+// const client = new Client({
+//   connectionString,
+// });
+
+// DATABASE_URL = postgresql://postgres:@localhost:5432/free_mentorsDB
 
 const client = new Client({
-  connectionString,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
+
+client.connect();
 
 module.exports = client;
