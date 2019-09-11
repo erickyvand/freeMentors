0517c401c0f17008b671226224d444e455bd2cb0
Ericky Vand <erickyvand@gmail.com>
2019-09-11 10:14:22 +0200

    add config file

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+client.connect();
+
+module.exports = client;
